64cb31a541512f567eafd6199916ef0177e495ca
BandiRamNiteeshReddy <143049772+BandiRamNiteeshReddy@users.noreply.github.com>
2023-11-10 11:44:30 -0600

    added

--- controllers/house.js
+++ controllers/house.js
@@ -13,9 +13,17 @@ exports.house_list = async function(req, res) {
     }
     };
 // for a specific house.
-exports.house_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: house detail: ' + req.params.id);
-};
+exports.house_detail = async function(req, res) {
+  console.log("detail" + req.params.id)
+  try {
+  result = await house.findById( req.params.id)
+  res.send(result)
+  } catch (error) {
+  res.status(500)
+  res.send(`{"error": document for id ${req.params.id} not found`);
+  }
+  };
+  
 // Handle house create on POST.
 exports.house_create_post = async function (req, res) {
     console.log(req.body);
@@ -23,7 +31,7 @@ exports.house_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"House_address":"goat", "House_colour":12, "House_capacity":"large"}
     document.House_address = req.body.House_address;
     document.House_colour = req.body.House_colour;
     document.House_capacity = req.body.House_capacity;
@@ -40,9 +48,28 @@ exports.house_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: house delete DELETE ' + req.params.id);
 };
 // Handle house update form on PUT.
-exports.house_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: house update PUT' + req.params.id);
-};
+exports.house_update_put = async function(req, res) {
+  console.log(`update on id ${req.params.id} with body
+  ${JSON.stringify(req.body)}`)
+  try {
+  let toUpdate = await house.findById( req.params.id)
+  // Do updates of properties
+  if(req.body.House_address)
+  toUpdate.House_address = req.body.House_address;
+  if(req.body.House_colour) toUpdate.House_colour = req.body.House_colour;
+  if(req.body.House_capacity) toUpdate.House_capacity = req.body.House_capacity;
+  if(req.body.checkboxsale) toUpdate.sale = true;
+else toUpdate.same = false;
+
+  let result = await toUpdate.save();
+  console.log("Sucess " + result)
+  res.send(result)
+  } catch (err) {
+  res.status(500)
+  res.send(`{"error": ${err}: Update for id ${req.params.id}
+  failed`);
+  }
+  };
 
 // VIEWS
 // Handle a show all view
--- routes/house.js
+++ routes/house.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const house_controlers= require('../controllers/house');
 var router = express.Router();
-/* GET houses */
-router.get('/', house_controlers.house_view_all_Page );
+// GET request for one house.
+router.get('/house/:id', house_controlers.house_detail)
 module.exports = router;
