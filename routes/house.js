451a4d26a92c39eb1c629c43f3c2c9cf49ef44a3
BandiRamNiteeshReddy <143049772+BandiRamNiteeshReddy@users.noreply.github.com>
2023-11-17 11:24:52 -0600

    added

--- routes/house.js
+++ routes/house.js
@@ -2,6 +2,8 @@ var express = require('express');
 const house_controlers= require('../controllers/house');
 var router = express.Router();
 
+router.get('/', house_controlers.house_view_all_Page );
+router.get('/detail', house_controlers.house_view_one_Page);
 /* GET create update page */
 router.get('/update', house_controlers.house_update_Page);
 
